f71874772ee23e662f49b1470e0a6eb43e17914f
Daniel Biacan <danielbiacaniii@gmail.com>
2023-06-27 21:18:49 +0800

    Added readme and boilerplate

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Computer chooses */
+let a = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  console.log(a[Math.floor(Math.random() * a.length)]);
+}
+getComputerChoice();
+
+/* Player chooses */
+
+let b = prompt("Choose");
+
+function playerChoice() {
+    if b = ("Rock")
+        alert("You chose Rock!")
+}
