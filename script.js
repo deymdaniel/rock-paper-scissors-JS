73c529dd7eb23fc0eeac38518c39aad9bbb04d53
Daniel Biacan <danielbiacaniii@gmail.com>
2023-06-27 23:07:38 +0800

    Added computer and player choice

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 /* Computer chooses */
-let a = ["Rock", "Paper", "Scissors"];
+let choice = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-  console.log(a[Math.floor(Math.random() * a.length)]);
+  return choice[Math.floor(Math.random() * a.length)];
 }
-getComputerChoice();
 
 /* Player chooses */
-
-let b = prompt("Choose");
+let a = "Rock";
+let b = "Paper";
+let c = "Scissors";
 
 function playerChoice() {
-    if b = ("Rock")
-        alert("You chose Rock!")
+  let playerChoice = prompt("Choose");
+
+  if (playerChoice.toLowerCase() === a.toLowerCase()) {
+    playerChoice = a;
+    console.log("You chose Rock!");
+  } else if (playerChoice.toLowerCase() === b.toLowerCase()) {
+    playerChoice = b;
+    console.log("You chose Paper!");
+  } else if (playerChoice.toLowerCase() === c.toLowerCase()) {
+    playerChoice = c;
+    console.log("You chose Scissors!");
+  }
 }
+
+function result(playerChoice, getComputerChoice) {
+  if (playerChoice === getComputerChoice) {
+    alert("Tie");
+  } else if ((a, "Paper")) {
+    alert("Computer chose paper, you lose");
+  } else if ((b, "Scissors")) {
+    alert("Computer chose scissors, you lose");
+  } else if ((c, "Rock")) {
+    alert("Computer chose rock, you lose");
+  } else {
+    alert("computer chose " + getComputerChoice() + " you win");
+  }
+}
+
+playerChoice();
+getComputerChoice();
+result(playerChoice, getComputerChoice);
