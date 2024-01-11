d47a6915bdf8bf8d9b13d0f562be4bf8f8a72b28
deymdaniel <danielbiacaniii@gmail.com>
2024-01-11 12:01:43 +0800

    Replace button ids and redo code

--- script.js
+++ script.js
@@ -1,94 +1,12 @@
-// constants
-
-const choices = ["rock", "paper", "scissors"];
-let computerChoice;
-let userChoice;
-let playerScore = 0;
-let computerScore = 0;
-
-// player choice
-
-function getPlayerChoice() {
-  userChoice = prompt(choices).toLowerCase();
-  if (userChoice == "rock") {
-    console.log("You chose " + userChoice);
-  } else if (userChoice == "paper") {
-    console.log("You chose " + userChoice);
-  } else if (userChoice == "scissors") {
-    console.log("You chose " + userChoice);
-  } else if (userChoice == "") {
-    console.log("Invalid Input!");
-  } else {
-    console.log("Invalid input!");
-  }
-}
-
 //computer choice
 
-function getComputerChoice() {
-  computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  console.log("Computer chose " + computerChoice);
-}
+// function getComputerChoice() {
+//   computerChoice = choices[Math.floor(Math.random() * choices.length)];
+//   console.log("Computer chose " + computerChoice);
+// }
 
-//find winner
-function getResult() {
-  if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    playerScore++;
-    console.log(
-      "You win! " +
-        "Player Score: " +
-        playerScore +
-        " Computer Score: " +
-        computerScore
-    );
-  } else if (userChoice === computerChoice) {
-    console.log("Its a tie!");
-  } else {
-    computerScore++;
-    console.log(
-      "Computer wins! " +
-        "Player Score: " +
-        playerScore +
-        " Computer Score: " +
-        computerScore
-    );
-  }
-}
+// Constants
 
-//round system
-
-function game() {
-  getPlayerChoice();
-  getComputerChoice();
-  getResult();
-
-  getPlayerChoice();
-  getComputerChoice();
-  getResult();
-
-  getPlayerChoice();
-  getComputerChoice();
-  getResult();
-
-  getPlayerChoice();
-  getComputerChoice();
-  getResult();
-
-  getPlayerChoice();
-  getComputerChoice();
-  getResult();
-
-  if (playerScore == computerScore) {
-    alert("Its a tie");
-  } else if (playerScore > computerScore) {
-    alert("YOU WIN!!!!");
-  } else {
-    alert("YOU LOSE");
-  }
-}
-
-game();
+const choices = [rock, paper, scissors];
+const playerChoice = "";
+const computerChoice = "";
