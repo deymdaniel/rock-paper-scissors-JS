d5aa360560c0239dc032962d54b9bbee54d0f549
deymdaniel <danielbiacaniii@gmail.com>
2024-01-11 13:48:37 +0800

    Added DOM methods for results and scores

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function game() {
   // Variables
 
   const choices = ["Rock", "Paper", "Scissors"];
+  const resultContent = document.querySelector("#result");
+  const winnerContent = document.querySelector("#winner");
+  const choicesContent = document.querySelector("#choices");
   let playerChoice = "";
   let computerChoice = "";
   let playerScore = 0;
@@ -11,7 +14,6 @@ function game() {
 
   function getComputerChoice() {
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log("Computer chose " + computerChoice);
   }
 
   //Player Choice
@@ -23,41 +25,76 @@ function game() {
     rockBtn.addEventListener("click", () => {
       playerChoice = "Rock";
       getComputerChoice();
-      console.log("You chose " + playerChoice);
-      Result();
+      const choice = document.createElement("p");
+      choice.textContent =
+        "You chose " + playerChoice + " | Computer chose " + computerChoice;
+      choicesContent.appendChild(choice);
+      //console.log("You chose " + playerChoice);
+      gameResult();
     });
     paperBtn.addEventListener("click", () => {
       playerChoice = "Paper";
       getComputerChoice();
-      console.log("You chose " + playerChoice);
-      Result();
+      const choice = document.createElement("p");
+      choice.textContent =
+        "You chose " + playerChoice + " | Computer chose " + computerChoice;
+      choicesContent.appendChild(choice);
+
+      //console.log("You chose " + playerChoice);
+      gameResult();
     });
     scissorsBtn.addEventListener("click", () => {
       playerChoice = "Scissors";
       getComputerChoice();
-      console.log("You chose " + playerChoice);
-      Result();
+      const choice = document.createElement("p");
+      choice.textContent =
+        "You chose " + playerChoice + " | Computer chose " + computerChoice;
+      choicesContent.appendChild(choice);
+
+      //console.log("You chose " + playerChoice);
+      gameResult();
     });
 
-    function Result() {
+    function gameResult() {
       if (
         (playerChoice === "Rock" && computerChoice === "Scissors") ||
         (playerChoice === "Paper" && computerChoice === "Rock") ||
         (playerChoice === "Scissors" && computerChoice === "Paper")
       ) {
         playerScore++;
-        console.log("Player: " + playerScore + " Computer: " + computerScore);
+        const scoreBoard = document.createElement("div");
+        scoreBoard.textContent =
+          "Player: " + playerScore + " Computer: " + computerScore;
+        resultContent.appendChild(scoreBoard);
+
+        //console.log("Player: " + playerScore + " Computer: " + computerScore);
       } else if (playerChoice === computerChoice) {
-        console.log("Its a tie");
+        const scoreBoard = document.createElement("div");
+        scoreBoard.textContent = "Its a Tie";
+        resultContent.appendChild(scoreBoard);
+
+        //console.log("Its a tie");
       } else {
         computerScore++;
-        console.log("Player: " + playerScore + " Computer: " + computerScore);
+        const scoreBoard = document.createElement("div");
+        scoreBoard.textContent =
+          "Player: " + playerScore + " Computer: " + computerScore;
+        resultContent.appendChild(scoreBoard);
+
+        //console.log("Player: " + playerScore + " Computer: " + computerScore);
       }
 
       if (playerScore == 5) {
-        console.log("YOU WIN");
+        const winner = document.createElement("h1");
+        winner.textContent = "YOU WIN!";
+        winnerContent.appendChild(winner);
+        //console.log("YOU WIN");
       } else if (computerScore == 5) {
-        console.log("YOU LOSE");
+        const winner = document.createElement("h1");
+        winner.textContent = "YOU LOSE!";
+        winnerContent.appendChild(winner);
+
+        //console.log("YOU LOSE");
       }
     }
   }
